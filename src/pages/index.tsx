16c2fe3215b118a33715c3ab15b8058a4d612b2d
Paulo Henrique <paulo-henrique-morada@hotmail.com>
2022-07-02 15:31:22 -0300

    Estrutura da aplicação - Nextjs, Sass, Document, Typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from '../../node_modules/next/head';
+
+export default function Home() {
+  return (
+      <>
+        <Head>
+            <title>Início ig.news</title>
+          </Head>
+        <h1>Hello worlds
+        </h1>
+      </>
+  )
+}
