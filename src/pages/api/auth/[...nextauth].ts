68c71235303eb1c367e288924a1615c47d16d7c6
Paulo Henrique <paulo-henrique-morada@hotmail.com>
2022-07-03 17:51:28 -0300

    Usando faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { signIn } from "next-auth/react"
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
 
@@ -9,4 +12,45 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({user, account, profile}){
+      const {email} = user;
+
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('USER_BY_EMAIL'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('USERS'),
+              {
+                data: { email: email } 
+              }
+            ),
+            q.Get(
+              q.Index('USER_BY_EMAIL'),
+              q.Casefold(user.email)
+            ),
+          )
+        )
+        .then((ret) => console.log(ret))
+        .catch((err) => console.error(
+          'Error: [%s] %s: %s',
+          err.name,
+          err.message,
+          err.errors()[0].description,
+        ));
+
+        return true;
+      }catch{
+        return false;
+      }      
+    }
+  },
 })
