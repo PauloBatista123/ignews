124c85f037436e85950ec31448650276440e107f
Paulo Henrique <paulo-henrique-morada@hotmail.com>
2022-07-04 21:39:35 -0300

    Ouvindo webhooks

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,53 @@
+import { fauna } from "../../../services/fauna";
+import {query as q} from 'faunadb'
+import { stripe } from "../../../services/stripe";
+
+export async function saveSubscriptions(subscriptionId: string, customerId: string, creteAction = false){
+  //buscar o usuário no banco do faunadb com o id customerId
+  //salvar os dados da subscription no faunadb
+
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(
+        q.Match(
+          q.Index('user_by_stripe_customer_id'),
+          customerId
+        )
+      )
+    )
+  )
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  }
+
+  if(creteAction){
+    await fauna.query(
+      q.Create(
+        q.Collection('subscription'),
+        { data: subscriptionData }
+      )
+    )
+  }else{
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(
+            q.Match(
+              q.Index('subscription_by_id'),
+              subscriptionId,
+            )
+          )
+        ),
+        { data: subscriptionData }
+      )
+    )
+  }
+}
